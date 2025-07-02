ae97059ba75d74a4386d9ec8e2e5001316a336c5
Odin Thif <mjd.thif@icloud.com>
2025-07-02 15:40:48 +0200

    init lira

---
+++ src/Lira.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Lira is ERC20 {
+    constructor(uint256 intialSupply) ERC20("Lira", "LIRA") {
+        _mint(msg.sender, intialSupply);
+    }
+}
